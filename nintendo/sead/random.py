d2339fa094062c0672aef0ce938572aa3f5aead3
Yannik Marchand <ymarchand@me.com>
2019-07-09 11:39:57 +0200

    Allow sead.Random to be constructed by internal state

--- nintendo/sead/random.py
+++ nintendo/sead/random.py
@@ -1,6 +1,12 @@
 
 class Random:
-	def __init__(self, seed):
+	def __init__(self, *param):
+		if len(param) == 1: self.set_seed(param[0])
+		elif len(param) == 4: self.set_state(*param)
+		else:
+			raise TypeError("Random.__init__ takes either 1 or 4 arguments")
+			
+	def set_seed(self, seed):
 		multiplier = 0x6C078965
 		
 		temp = seed
@@ -9,6 +15,9 @@ class Random:
 			temp ^= temp >> 30
 			temp = (temp * multiplier + i) & 0xFFFFFFFF
 			self.state.append(temp)
+			
+	def set_state(self, s0, s1, s2, s3):
+		self.state = [s0, s1, s2, s3]
 
 	def u32(self):
 		temp = self.state[0]
