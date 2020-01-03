7a01b21ab161a29051b05dcfa39ed54534ed5e6a
Yannik Marchand <ymarchand@me.com>
2020-01-03 13:05:48 +0100

    Add missing modules to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
 	name = "Nintendo",
 	description = "Client for Nintendo game servers",
 	url = "https://github.com/Kinnay/NintendoClients",
-	packages = ["nintendo", "nintendo.common", "nintendo.nex", "nintendo.pia"],
+	packages = [
+		"nintendo", "nintendo.common", "nintendo.enl",
+		"nintendo.nex", "nintendo.pia", "nintendo.sead"
+	],
 	package_data = {"nintendo": ["files/config/*", "files/cert/*"]}
 )
