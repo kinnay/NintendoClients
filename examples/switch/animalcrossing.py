9ef3629662575d044249a45d91a2f52f8221001a
Yannik Marchand <ymarchand@me.com>
2021-09-28 09:22:08 +0200

    Update system version in example scripts

--- examples/switch/animalcrossing.py
+++ examples/switch/animalcrossing.py
@@ -11,7 +11,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-SYSTEM_VERSION = 1210 #12.1.0
+SYSTEM_VERSION = 1300 #13.0.0
 
 # You can get your user id and password from
 # su/baas/<guid>.dat in save folder 8000000000000010.
