be4583b4460ce6aa41e2a38a3e462402545d62c0
Yannik Marchand <ymarchand@me.com>
2022-01-18 13:59:48 +0100

    Various changes related to request parameters

--- examples/3ds/friends.py
+++ examples/3ds/friends.py
@@ -9,30 +9,35 @@ logging.basicConfig(level=logging.INFO)
 
 
 SERIAL_NUMBER = "..." # Serial number on console minus the last digit
-MAC_ADDRESS = "..." # Console MAC address (see WiFi settings), all lowercase with no colons
-FCDCERT = "..." # Unique console certificate. Get from sniffing traffic
-
-PID_HMAC = "..." # Sniff console traffic or dump from friends title save (bytes 66-84)
+MAC_ADDRESS = "aabbccddeeff" # Console MAC address (see WiFi settings), all lowercase with no colons
+DEVICE_CERT = bytes.fromhex("...") # Unique console certificate. Get from sniffing traffic
+DEVICE_NAME = "..." # Doesn't matter
 
 # 3DS does NOT send NEX credentials over NASC
 # They are generated once when the account is created and stored on the device
 # Homebrew like https://github.com/Stary2001/nex-dissector/tree/master/get_3ds_pid_password
 # can be used to dump the PID and password
 PID = 0
+PID_HMAC = "..." # Sniff console traffic or dump from friends title save (bytes 66-84)
+
 NEX_PASSWORD = "..."
+
+REGION = 3 # EUR
+LANGUAGE = 2
 
 
 async def main():
 	client = nasc.NASCClient()
-
-	client.set_device(SERIAL_NUMBER, MAC_ADDRESS, FCDCERT)
+	client.set_title(Friends3DS.TITLE_ID_EUR, Friends3DS.LATEST_VERSION)
+	client.set_device(SERIAL_NUMBER, MAC_ADDRESS, DEVICE_CERT, DEVICE_NAME)
+	client.set_locale(REGION, LANGUAGE)
 	client.set_user(PID, PID_HMAC)
-	client.set_title(Friends3DS.TITLE_ID_EUR, Friends3DS.LATEST_VERSION, Friends3DS.PRODUCT_CODE_EUR)
-	nasc_data = await client.get_nasc_data(Friends3DS.GAME_SERVER_ID)
+	
+	response = await client.login(Friends3DS.GAME_SERVER_ID)
 
 	s = settings.load("friends")
 	s.configure(Friends3DS.ACCESS_KEY, Friends3DS.NEX_VERSION)
-	async with backend.connect(s, nasc_data.locator.host, nasc_data.locator.port) as be:
+	async with backend.connect(s, response.host, response.port) as be:
 		async with be.login(str(PID), NEX_PASSWORD) as client:
 			friends_client = friends.FriendsClientV1(client)
 			await friends_client.update_comment("Hello World")
--- nintendo/nasc.py
+++ nintendo/nasc.py
@@ -2,6 +2,7 @@
 from anynet import http, tls
 import pkg_resources
 import datetime
+import secrets
 import base64
 
 import logging
@@ -13,11 +14,20 @@ CERT = pkg_resources.resource_filename("nintendo", "files/cert/ctr-common-1-cert
 KEY = pkg_resources.resource_filename("nintendo", "files/cert/ctr-common-1-key.der")
 
 
+MEDIA_TYPE_SYSTEM = 0
+MEDIA_TYPE_DIGITAL = 1
+MEDIA_TYPE_CARTRIDGE = 2
+
+
 def b64decode(text):
 	text = text.replace(".", "+").replace("-", "/").replace("*", "=")
-	return base64.b64decode(text).decode()
+	return base64.b64decode(text)
 
 def b64encode(text):
+	# Convert to bytes if necessary
+	if isinstance(text, str):
+		text = text.encode()
+	
 	text = base64.b64encode(text).decode()
 	return text.replace("+", ".").replace("/", "-").replace("=", "*")
 
@@ -27,50 +37,45 @@ def decode_form(form):
 def encode_form(form):
 	return {key: b64encode(value) for key, value in form.items()}
 
+def parse_date(text):
+	return datetime.datetime.strptime(text, "%Y%m%d%H%M%S")
+
+
+# Known error codes:
+#	001: success
+#   109: missing or malformed parameter in request
+#   110: game server is no longer available
+#   121: device certificate is invalid
+#   122: uid hmac is invalid
+#   125: game id is invalid
 
 class NASCError(Exception):
 	def __init__(self, status_code, form):
 		self.status_code = status_code
-		self.form = form
+		
+		self.return_code = int(form["returncd"].decode())
+		self.retry = bool(int(form["retry"].decode()))
+		self.datetime = parse_date(form["datetime"].decode())
 	
 	def __str__(self):
-		if "returncd" in self.form:
-			return "NASC request failed: %s" %self.form["returncd"]
-		else:
-			return "NASC request failed with status %i" %self.status_code
+		return "NASC request failed with error code %i" %self.return_code
 
 
-class NASCLocator:
-	def __init__(self):
-		self.host = None
-		self.port = None
-
-	@classmethod
-	def parse(cls, locator):
-		host, port = locator.split(":")
-
-		inst = cls()
-		inst.host = host
-		inst.port = port
-		return inst
-
-
-class NASCResponse:
+class LoginResponse:
 	def __init__(self):
 		self.locator = None
-		self.retry = None
-		self.returncd = None
 		self.token = None
 		self.datetime = None
 
 	@classmethod
 	def parse(cls, form):
+		host, port = form["locator"].decode().split(":")
+		
 		inst = cls()
-		inst.locator = NASCLocator.parse(form.get("locator"))
-		inst.retry = form.get("retry")
-		inst.return_code = form.get("returncd")
-		inst.token = form.get("token")
-		inst.datetime = form.get("datetime")
+		inst.host = host
+		inst.port = int(port)
+		inst.token = form["token"]
+		inst.datetime = parse_date(form["datetime"].decode())
 		return inst
 
 
@@ -86,48 +91,80 @@ class NASCClient:
 		self.context.set_authority(ca)
 		self.context.set_certificate(cert, key)
 		
-		self.game_server_id = None
+		self.sdk_version_major = 0
+		self.sdk_version_minor = 0
+		
 		self.title_id = None
 		self.title_version = None
-		self.title_product_code = None
-
-		self.fcdcert = None
+		self.product_code = "----"
+		self.maker_code = "00"
+		self.media_type = MEDIA_TYPE_SYSTEM
+		self.rom_id = None
+		
 		self.serial_number = None
-		self.device_name = "NintendoClients" # Doesn't matter
-		self.current_access_point_slot = "01:0000000000"  # Doesn't matter
 		self.mac_address = None
-		self.bss_id = ""  # Doesn't matter
-		self.fpd_version = "000F"
-		self.environment = "L1"
-
-		# These don't matter
-		self.sdk_version = "000000"
-		self.media_type = "0"
-		self.maker_cd = "00"
-		self.unit_cd = "2"
+		self.device_cert = None
+		self.device_name = ""
+		self.unit_code = "2"
+		
+		self.bss_id = secrets.token_hex(6)
+		self.ap_info = "01:0000000000"
+		
+		self.region = 3
+		self.language = 2
 		
 		self.pid = None
 		self.pid_hmac = None
+		self.password = None
 		
-		self.language = "01"
-		self.region = "01"
+		self.fpd_version = 15
+		self.environment = "L1"
 	
-	def set_device(self, serial_number, mac_address, fcdcert):
+	def set_sdk_version(self, major_version, minor_version):
+		self.sdk_major_version = major_version
+		self.sdk_minor_version = minor_version
+	
+	def set_title(
+		self, title_id, title_version, product_code="----", maker_code="00",
+		media_type=MEDIA_TYPE_SYSTEM, rom_id=None
+	):
+		if media_type == MEDIA_TYPE_CARTRIDGE and rom_id is None:
+			raise ValueError("Rom ID is required for cartridge media type")
+		
+		self.title_id = title_id
+		self.title_version = title_version
+		self.product_code = product_code
+		self.maker_code = maker_code
+		self.media_type = media_type
+		self.rom_id = rom_id
+	
+	def set_device(self, serial_number, mac_address, certificate, name="", unit_code="2"):
 		self.serial_number = serial_number
 		self.mac_address = mac_address
-		self.fcdcert = fcdcert
-		
+		self.device_cert = certificate
+		self.device_name = name
+		self.unit_code = unit_code
+	
+	def set_network(self, bss_id, ap_info):
+		self.bss_id = bss_id
+		self.ap_info = ap_info
+	
 	def set_locale(self, region, language):
 		self.region = region
 		self.language = language
+	
+	def set_user(self, pid, pid_hmac):
+		self.pid = pid
+		self.pid_mac = pid_hac
+		self.password = None
 		
+	def set_password(self, password):
+		self.pid = None
+		self.pid_hmac = None
+		self.password = password
+	
 	def set_fpd_version(self, version): self.fpd_version = version
 	def set_environment(self, environment): self.environment = environment
-	
-	def set_title(self, title_id, title_version, title_product_code):
-		self.title_id = title_id
-		self.title_version = title_version
-		self.title_product_code = title_product_code
 
 	def set_user(self, pid, pid_hmac):
 		self.pid = pid
@@ -136,55 +173,67 @@ class NASCClient:
 	async def request(self, req):
 		# Apply Nintendo's custom base64 encoding
 		req.form = encode_form(req.form)
+		print(req.encode().decode())
 		
 		# Must manually decode form here since server doesn't return correct content-type
 		response = await http.request(self.url, req, self.context)
 		response.form = decode_form(http.formdecode(response.text))
 		
-		if response.error():
+		return_code = int(response.form["returncd"].decode())
+		if return_code != 1:
 			raise NASCError(response.status_code, response.form)
-
-		if not response.form.get("locator"):
-			raise NASCError(response.status_code, response.form)
-
+		
 		return response
 
-	async def login(self, game_server_id):
+	async def login(self, game_server_id, ingamesn=""):
+		if self.title_id is None:
+			raise ValueError("Please configure the title (set_title)")
+		if self.serial_number is None:
+			raise ValueError("Please configure the device (set_device)")
+		if self.pid is None and self.passwd is None:
+			raise ValueError("Please configure a user id or password (set_user / set_password)")
+		
 		req = http.HTTPRequest.post("/ac")
 		req.headers["Host"] = self.url
-		req.headers["X-GameId"] = "%08X" % game_server_id
-		req.headers["User-Agent"] = "CTR FPD/%s" %self.fpd_version
-		req.headers["Content-Type"] = "application/x-www-form-urlencoded"
+		req.headers["X-GameId"] = "%08X" %game_server_id
+		req.headers["User-Agent"] = "CTR FPD/%04X" %self.fpd_version
+		
+		# The real 3DS seems to add this header twice for some reason
+		req.headers.add("Content-Type", "application/x-www-form-urlencoded")
+		req.headers.add("Content-Type", "application/x-www-form-urlencoded")
 
 		now = datetime.datetime.now()
 		device_time = now.strftime("%y%m%d%H%M%S")
 
-		req.form = {
-			"gameid": "%08X" %game_server_id,
-			"sdkver": self.sdk_version,
-			"titleid": "%016X" % self.title_id,
-			"gamecd":  self.title_product_code,
-			"gamever": "%04X" % self.title_version,
-			"mediatype": self.media_type,
-			"makercd": self.maker_cd,
-			"unitcd": self.unit_cd,
-			"macadr": self.mac_address,
-			"bssid": self.bss_id,
-			"apinfo": self.current_access_point_slot,
-			"fcdcert": self.fcdcert,
-			"devname": self.device_name,
-			"servertype": self.environment,
-			"fpdver": self.fpd_version,
-			"devtime": device_time,
-			"lang": self.language,
-			"region": self.region,
-			"csnum": self.serial_number,
-			"uidhmac": self.pid_hmac,
-			"userid": "%i" % self.pid,
-			"action": "LOGIN",
-			"ingamesn": ""
-		}
-
-		response = await self.request(req)
+		req.form = {}
+		req.form["gameid"] = "%08X" %game_server_id
+		req.form["sdkver"] = "%03i%03i" %(self.sdk_version_major, self.sdk_version_minor)
+		req.form["titleid"] = "%016X" %self.title_id
+		req.form["gamecd"] = self.product_code
+		req.form["gamever"] = "%04X" %self.title_version
+		req.form["mediatype"] = str(self.media_type)
+		if self.media_type == MEDIA_TYPE_CARTRIDGE:
+			req.form["romid"] = self.rom_id
+		req.form["makercd"] = self.maker_code
+		req.form["unitcd"] = self.unit_code
+		req.form["macadr"] = self.mac_address
+		req.form["bssid"] = self.bss_id
+		req.form["apinfo"] = self.ap_info
+		req.form["fcdcert"] = self.device_cert
+		req.form["devname"] = self.device_name.encode("utf-16-le")
+		req.form["servertype"] = self.environment
+		req.form["fpdver"] = "%04X" %self.fpd_version
+		req.form["devtime"] = device_time
+		req.form["lang"] = "%02X" %self.language
+		req.form["region"] = "%02X" %self.region
+		req.form["csnum"] = self.serial_number
+		if self.pid_hmac is not None:
+			req.form["uidhmac"] = self.pid_hmac
+			req.form["userid"] = str(self.pid)
+		else:
+			req.form["passwd"] = self.password
+		req.form["action"] = "LOGIN"
+		req.form["ingamesn"] = ingamesn
 		
-		return NASCResponse.parse(response.form)
+		response = await self.request(req)
+		return LoginResponse.parse(response.form)
