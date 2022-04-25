6bf43e5f146bda41876cf96febbf2c3c22166142
Yannik Marchand <ymarchand@me.com>
2022-04-25 17:37:09 +0200

    Push version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = \
 
 setuptools.setup(
 	name = "nintendoclients",
-	version = "0.0.1",
+	version = "0.0.2",
 	description = "Nintendo network library",
 	long_description = long_description,
 	author = "Yannik Marchand",
