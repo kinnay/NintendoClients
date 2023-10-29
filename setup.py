6223c43c152c415e94780e464daecd464f82d33f
Yannik Marchand <ymarchand@me.com>
2023-10-29 17:27:18 +0100

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = \
 
 setuptools.setup(
 	name = "nintendoclients",
-	version = "1.1.0",
+	version = "2.0.0",
 	description = "Nintendo network library",
 	long_description = long_description,
 	author = "Yannik Marchand",
