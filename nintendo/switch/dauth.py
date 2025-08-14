dd7c49c177d5f8a44ec87a8017e81581b38157f9
TheGreatRambler <tgr@tgrcode.com>
2025-08-13 18:40:00 -0600

    Only index into `results` if it exists

--- nintendo/switch/dauth.py
+++ nintendo/switch/dauth.py
@@ -456,10 +456,11 @@ class DAuthClient:
 		token_requests = [{"client_id": "%016x" %client_id} for client_id in client_ids]
 		response = await self.request_tokens(token_requests, edge_tokens=False)
 
-		for result in response["results"]:
-			if "error" in result:
-				logger.error("  (%s) %s", result["error"]["code"], result["error"]["message"])
-				raise DAuthError(result["error"])
+		if "results" in response:
+			for result in response["results"]:
+				if "error" in result:
+					logger.error("  (%s) %s", result["error"]["code"], result["error"]["message"])
+					raise DAuthError(result["error"])
 
 		return response
 	
@@ -467,10 +468,11 @@ class DAuthClient:
 		token_requests = [{"client_id": "%016x" %client_id, "vendor_id": vendor_id} for client_id, vendor_id in token_requests]
 		response = await self.request_tokens(token_requests, edge_tokens=True)
 
-		for result in response["results"]:
-			if "error" in result:
-				logger.error("  (%s) %s", result["error"]["code"], result["error"]["message"])
-				raise DAuthError(result["error"])
+		if "results" in response:
+			for result in response["results"]:
+				if "error" in result:
+					logger.error("  (%s) %s", result["error"]["code"], result["error"]["message"])
+					raise DAuthError(result["error"])
 
 		return response
 	
